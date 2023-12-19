fb8dc880c0fe4512c306343c6b1c4aa4d4eb29a1
alex-njuguna <njugunakinuthia013@gmail.com>
2023-12-20 00:57:52 +0300

    home page styling

--- client/src/components/ShowProducts.jsx
+++ client/src/components/ShowProducts.jsx
@@ -20,15 +20,16 @@ const ShowProducts = () => {
         <div className='row'>
             {products.map((product) => (
                 <div className='col-md-2'>
-                <div className="card m-2 pb-2 text-center rounded shadow-lg">
+                <div className="card m-2 pb-2 text-center rounded shadow-lg text-capitalize">
                     <div className="card-img-top" >
                         <img className='w-100 p-1' src={product.image}  alt={product.name} />
                     </div>
                     <div className="card-body p-2">
-                        <h5 style={{textDecoration: 'underline'}}>{product.name}</h5>
-                        <p>{(product.category) ? product.category : 'N/A'}</p>
-                        <p>{product.price}</p>
-                        <p>{product.description}</p>
+                        <span className='fs-5 '>{product.name}</span>
+                        <span className='text-success fs-6'><em> ({(product.category) ? product.category : 'N/A'})</em></span> &nbsp;
+                        <br />
+                        <span className='text-danger'>@ {product.price}</span> &nbsp;
+                        <span className='text-info'> - {product.description}</span>
                     </div>
                     <div className="card-foter">
                         <button className='btn btn-primary'>View</button>

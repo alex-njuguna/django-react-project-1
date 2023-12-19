acc9a9ae94269e3534f089af0313cb7ed50343d7
alex-njuguna <njugunakinuthia013@gmail.com>
2023-12-19 23:36:29 +0300

    new migrations

--- server/products/models.py
+++ server/products/models.py
@@ -5,7 +5,7 @@ image, name, price, category, description"""
 class Product(models.Model):
     image = models.ImageField(upload_to='images')
     name = models.CharField(max_length=50)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
     category = models.CharField(max_length=50, blank=True)
     description = models.TextField()
 
